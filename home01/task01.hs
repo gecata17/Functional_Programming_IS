3c7efbc37f177d5c6821e77c586622663bc1e225
Georgi Birdanov <72568555+gecata17@users.noreply.github.com>
2021-03-03 11:35:14 +0200

    Update task01.hs

--- home01/task01.hs
+++ home01/task01.hs
@@ -1,16 +1,16 @@
 main :: IO()
 main = do
-    print $ isEvenIf 2 == True
-    print $ isEvenIf 15452 == True
-    print $ isEvenIf 321 == False
-    print $ isEvenGuards 2 == True
-    print $ isEvenGuards 15452 == True
-    print $ isEvenGuards 321 == False
+    print $ isEvenIf 2 == "Yes"
+    print $ isEvenIf 15452 == "Yes"
+    print $ isEvenIf 321 == "No"
+    print $ isEvenGuards 2 == "Yes"
+    print $ isEvenGuards 15452 == "Yes"
+    print $ isEvenGuards 321 == "No"
 
-isEvenIf :: Int -> Bool
-isEvenIf n = if mod n 2 == 0 then True else  False
+isEvenIf :: Int -> String 
+isEvenIf n = if mod n 2 == 0 then "Yes" else "No"
 
-isEvenGuards :: Int -> Bool 
+isEvenGuards :: Int -> String  
 isEvenGuards n
- |mod n 2 == 0 = True 
- |otherwise = False
+ |mod n 2 == 0 = "Yes"
+ |otherwise = "No"
