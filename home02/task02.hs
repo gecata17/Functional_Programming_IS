af5acd82c11ee049470ca72b653fac6563dc69d9
Georgi Birdanov <72568555+gecata17@users.noreply.github.com>
2021-03-11 14:27:00 +0200

    Update task02.hs

--- home02/task02.hs
+++ home02/task02.hs
@@ -1,18 +1,16 @@
 main :: IO ()
 main = do
-    print $ sumDigitsIter 9 -- == 9
-    print $ sumDigitsIter 12345  --15 
-    print $ sumDigitsIter 123 -- 6
+    print $ sumDigitsIter 9  == 9
+    print $ sumDigitsIter 12345  ==15 
+    print $ sumDigitsIter 123 == 6
     print $ sumDigitsIter (-13) -- error "n was negative" 
 
 
 sumDigitsIter :: Int -> Int
-sumDigitsIter n =helper n 1
+sumDigitsIter n =helper n 0
   where
       helper :: Int -> Int -> Int
       helper n result
        | n < 0 = error "n was negative"
-       | n < 10 = n
-       |otherwise = helper (result*(mod n 10)) (div n 10)
-       
-  --not okay with test cases
+       | n < 10 = result + n
+       |otherwise = helper   (div n 10) (result+(mod n 10))
