8afa5ad136a80940f6da3906a8c8fd74373df865
Georgi Birdanov <72568555+gecata17@users.noreply.github.com>
2021-03-12 16:21:46 +0200

    Update task14.hs

--- home02/task14.hs
+++ home02/task14.hs
@@ -1,10 +1,10 @@
 main :: IO()
 main = do
-    print $ isSquare 1 == True
-    print $ isSquare 2 == False
+    print $ isSquare 1 ==True  
+    print $ isSquare 2 ==False
     print $ isSquare 4 == True
-    print $ isSquare 17 == False
-    print $ isSquare 256 == True
+    print $ isSquare 17 ==False
+    print $ isSquare 256 == True 
     print $ isSquare 2500 == True
 
 isSquare :: Int -> Bool 
@@ -12,9 +12,10 @@ isSquare 0 = False
 isSquare 1 = True
 isSquare n 
  |n < 0 = error "n was negative"
- |otherwise  =helper 1
+ |otherwise = helper 1
   where
       helper ::Int -> Bool
       helper currentDivisor
-       |div n currentDivisor >=currentDivisor = True 
+       |currentDivisor >= div n 2 + 1 = False
+       | currentDivisor * currentDivisor == n = True 
        |otherwise = helper(currentDivisor +1)
