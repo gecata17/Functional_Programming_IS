b67545a6f22c85981bbfb023879e948910ec8645
Georgi Birdanov <72568555+gecata17@users.noreply.github.com>
2021-03-15 02:36:44 +0200

    Update task13.hs

--- home02/task13.hs
+++ home02/task13.hs
@@ -1,34 +1,26 @@
 main :: IO()
 main = do
-    print $ findSum 0 2 10 == 3578 -- 510 + 1022 + 2046 
+    print $ findSum 0 2 10 == 3578-- 510 + 1022 + 2046 
     print $ findSum 5 3 5 == 174 -- 26 + 50 + 98
 
 firstSum :: Int -> Int -> Int -> Int 
-firstSum a b n
- |n <= 3 = error "n is not right"
- |otherwise = helper a b n 1
+firstSum a b n = a + helper n 0
  where   
-     helper :: Int -> Int -> Int -> Int -> Int
-     helper firstNum secondNum thirdNum result=helper (result+firstNum) ((2^(result+1) )*secondNum) (thirdNum) result
+     helper 0 _ = 0
+     helper leftOver power = 2^power*b + helper(leftOver-1) (power+1)
     
 secondSum :: Int -> Int -> Int -> Int 
-secondSum  a b n
- |n <= 3 = error "n is not right"
- |otherwise = helper a b n 1
+secondSum a b n = a + helper (n-1) 0
  where   
-     helper :: Int -> Int -> Int -> Int -> Int
-     helper firstNum secondNum thirdNum result=helper (result+firstNum) ((2^(result+1) )*secondNum) (thirdNum-1) result
+     helper 0 _ = 0
+     helper leftOver power = 2^power*b + helper(leftOver-1) (power+1)
     
 thirdSum :: Int -> Int -> Int -> Int 
-thirdSum  a b n
- |n <= 3 = error "n is not right"
- |otherwise = helper a b n 1
+thirdSum a b n = a + helper (n-2) 0
  where   
-     helper :: Int -> Int -> Int -> Int -> Int
-     helper firstNum secondNum thirdNum result=helper (result+firstNum) ((2^(result+1) )*secondNum) (thirdNum-2) result
+     helper 0 _ = 0
+     helper leftOver power = 2^power*b + helper(leftOver-1) (power+1)
     
 
-
-
 findSum :: Int -> Int -> Int -> Int 
-findSum x y z = firstSum x y z + secondSum x y z + thirdSum x y z
+findSum x y z = firstSum x y z + secondSum x y z +thirdSum x y z 
