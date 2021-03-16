b9710a45bad8c2ece9b160987c3ff84142af1e33
Georgi Birdanov <72568555+gecata17@users.noreply.github.com>
2021-03-17 01:10:08 +0200

    Create task01.hs

---
+++ week03_(12-03)/task01.hs
@@ -0,0 +1,7 @@
+main:: IO()
+main = do
+    print $ isNarcissistic 7 == True
+    print $ isNarcissistic 12 == False
+    print $ isNarcissistic 370 == True
+    print $ isNarcissistic 371 == True
+    print $ isNarcissistic 1634 == True    
