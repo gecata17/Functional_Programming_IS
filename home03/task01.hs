4187558d3e6e5f009d4ff99bbd03b8d6631a4d9a
Georgi Birdanov <72568555+gecata17@users.noreply.github.com>
2021-03-19 22:40:17 +0200

    Update task01.hs

--- home03/task01.hs
+++ home03/task01.hs
@@ -2,11 +2,11 @@ main :: IO()
 main = do 
     print $ digital_root(9) == 9
     print $ digital_root(16) == 7 -- => 1 + 6 => 7 
-    print $ digital_root(78) == 15
+    print $ digital_root(78) == 6
     print $ digital_root (463) == 4
     print $ digital_root(942)  == 6 -- => 9 + 4 + 2 => 15 ... => 1 + 5 => 6 
-    print $ digital_root(132189) -- == 6-- => 1 + 3 + 2 + 1 + 8 + 9 => 24 ... => 2 + 4 => 6 
-    print $ digital_root(493193) -- == 2 --  => 4 + 9 + 3 + 1 + 9 + 3 => 29 ... => 2 + 9 => 11 ... => 1 + 1 => 2
+    print $ digital_root(132189) == 6-- => 1 + 3 + 2 + 1 + 8 + 9 => 24 ... => 2 + 4 => 6 
+    print $ digital_root(493193)  == 2 --  => 4 + 9 + 3 + 1 + 9 + 3 => 29 ... => 2 + 9 => 11 ... => 1 + 1 => 2
 
 sumDigitsRec :: Int -> Int 
 sumDigitsRec n 
@@ -17,6 +17,5 @@ sumDigitsRec n
 digital_root :: Int->Int 
 digital_root n 
  |n < 0 = error "n was negative "
- |n < 10 =  n
- |n < 100 = sumDigitsRec n
- | otherwise = digital_root( mod n 10) + sumDigitsRec (digital_root (div n 10))
+ |n < 10 = n 
+ | otherwise = digital_root $ sumDigitsRec n 
