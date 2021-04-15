9b3b11a4bf6eac7a0226a4f0f0ed4c28bfe0edf3
Georgi Birdanov <72568555+gecata17@users.noreply.github.com>
2021-04-15 20:11:51 +0300

    Update task01.hs

--- exam1/task01.hs
+++ exam1/task01.hs
@@ -1,1 +1,33 @@
 
+main :: IO()
+main = do 
+
+    print $ sumofEvenly 1 10  == 41 --2+3+5+6+7+8+10
+    print $ sumofEvenly 5 20  == 175 --5+6+7+8+10+11+12+13+14+15+17+18+19+20
+    print $ sumofEvenly 3 20  == 178
+
+countDivisors::Int->Int 
+countDivisors 0 = 0
+countDivisors n 
+ |n < 1 = error "n was not natural"
+ |otherwise = helper 1
+  where
+      helper :: Int->Int
+      helper currDivisor
+       |currDivisor>=n = 1
+       |mod n currDivisor == 0 = 1 + helper (currDivisor+1)
+       |otherwise = helper (currDivisor+1)
+
+isEvenly::Int->Bool 
+isEvenly n 
+ |mod(countDivisors n ) 2 == 0 =True
+ |otherwise = False
+
+sumofEvenly :: Int -> Int -> Int 
+sumofEvenly a b = helper (min a b) ((max a b ) +1)
+ where
+     helper start finish
+      |start >= finish = 0
+      |isEvenly start= start + helper (start + 1) finish 
+      |otherwise = helper (start + 1) finish
+    
