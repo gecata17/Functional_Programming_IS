a9fc438329a7f339ae70e6b95e3200a001068248
Georgi Birdanov <72568555+gecata17@users.noreply.github.com>
2021-10-04 22:23:37 +0300

    Delete home07 directory

--- home07/task00.hs
+++
@@ -1,4 +0,0 @@
-main :: IO()
-main = do
-    print $ map (\ (_, fn, _) -> fn) [("Simo", 71845, 5), ("Yoncho", 71865, 6)]
-    print $ length $ filter (\ (_, _, gr) -> gr >= 4.50 && gr < 5.50) [("Simo", 71845, 5), ("Yoncho", 71865, 6)]
